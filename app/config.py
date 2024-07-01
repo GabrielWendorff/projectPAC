f70ed5691198943157c1af555ce09c535618c553
guifreiberger <guigafreiberger@gmail.com>
2024-07-01 14:34:44 -0300

    01-07-2024

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:@127.0.0.1/teste_costura')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:root@127.0.0.1/teste_costura')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
